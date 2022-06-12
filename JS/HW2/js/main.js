ca7b3ad307bc8c94bebb328772f53491463f32b6
Vladyslav <povalii0312@gmail.com>
2022-06-12 18:42:59 +0200

    hw2

--- JS/HW2/js/main.js
+++ JS/HW2/js/main.js
@@ -1,13 +1,24 @@
 "use strict"
 
-let userNumber;
-userNumber = prompt ("Enter your number");
-while (isNaN(userNumber) || userNumber === "" || userNumber === null) {
-	userNumber = prompt ("Enter the number!")
+let currentUserName, currentUserAge;
+let warning;
+currentUserName = prompt("Enter you name");
+while (+currentUserName || currentUserName === null || currentUserName === "") {
+	currentUserName = prompt("Enter you name!");
 }
-alert (`Your number is ${userNumber}`)
-for (let i = 0; i < +userNumber; i++) {
-	if (i % 5 === 0) {
-		console.log(i);
+currentUserAge = prompt("Enter your age");
+while (isNaN(currentUserAge) || currentUserAge === "" || currentUserAge === null) {
+	currentUserAge = prompt("Enter your age!");
+}
+if (currentUserAge < 18) {
+	alert("You are not allowed to visit this website");
+} else if (currentUserAge > 18 && currentUserAge < 22) {
+	warning = confirm("Are you sure you want to continue?");
+	if (warning) {
+		alert(`Welcome ${currentUserName}`);
+	} else {
+		alert("You are not allowed to visit this website");
 	}
+} else if (currentUserAge > 22) {
+	alert(`Welcome ${currentUserName}`);
 }
