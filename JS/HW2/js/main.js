3966cde824d12d07359529c29f792ba4717ec9db
Vladyslav <povalii0312@gmail.com>
2022-09-09 02:24:32 +0200

    HW2

--- JS/HW2/js/main.js
+++ JS/HW2/js/main.js
@@ -1,24 +1,33 @@
-"use strict"
+/*
+1. Какие существуют типы данных в Javascript?
+	Примитивные - string, number (+infinity, NaN), boolean, null, undefined, symbol;
+	Комплексный - object;
+2. В чем разница между == и === ?
+	Оператор нестрогого равенства - проверяет равенство без привидения типов.
+	Оператор строгого равенства - проверяет равенство с привидением типов.
+3. Что такое оператор?
+	Оператор - математический знаки со своими свойствами, которые  основном находятся между операндами. Бинарный и унарный. 
+*/
 
-let currentUserName, currentUserAge;
-let warning;
-currentUserName = prompt("Enter you name");
-while (+currentUserName || currentUserName === null || currentUserName === "") {
-	currentUserName = prompt("Enter you name!");
+let userName = prompt('Enter your name');
+if (userName === '' || userName === null || +userName) {
+	userName = prompt('Enter your name please!');
 }
-currentUserAge = prompt("Enter your age");
-while (isNaN(currentUserAge) || currentUserAge === "" || currentUserAge === null) {
-	currentUserAge = prompt("Enter your age!");
+
+let userAge = prompt('Enter your age');
+if (isNaN(userAge) || userAge === '' || userAge === null) {
+	userAge = prompt('Enter you age please!');
 }
-if (currentUserAge < 18) {
-	alert("You are not allowed to visit this website");
-} else if (currentUserAge > 18 && currentUserAge < 22) {
-	warning = confirm("Are you sure you want to continue?");
-	if (warning) {
-		alert(`Welcome ${currentUserName}`);
+
+if (userAge <= 17) {
+	alert('You are not allowed to visit this website');
+} else if (userAge >= 18 && userAge <= 22) {
+	let userAnswer = confirm('Are you sure you want to continue?')
+	if (userAnswer) {
+		alert(`Welcome, ${userName}`);
 	} else {
-		alert("You are not allowed to visit this website");
+		alert('You are not allowed to visit this website');
 	}
-} else if (currentUserAge > 22) {
-	alert(`Welcome ${currentUserName}`);
+} else {
+	alert(`Welcome, ${userName}`);
 }
