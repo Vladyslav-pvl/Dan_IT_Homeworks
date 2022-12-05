73b7c0e183260262b49df63525fcd6675444247e
Vladyslav <povalii0312@gmail.com>
2022-12-05 23:18:33 +0100

    sth

--- HTML,CSS_ADVANCED/forkio/src/scripts/script.js
+++ HTML,CSS_ADVANCED/forkio/src/scripts/script.js
@@ -5,11 +5,25 @@ const line2 = document.querySelector('.line2');
 const line3 = document.querySelector('.line3');
 
 
-btn.addEventListener('click', () => {
-    nav.classList.toggle('disactive');
-    if (btn.classList !== 'disactive') {
+
+btn.addEventListener('click', (el) => {
+    if (el.target.dataset.path === "button-el") {
+        nav.classList.toggle('disactive');
         line1.classList.toggle('hide-line');
         line2.classList.toggle('active2');
-        line3.classList.toggle('active3')
+        line3.classList.toggle('active3');
     }
+
+    window.addEventListener('click', e => {
+        if (e.target.dataset.path !== "button-el" && e.target.dataset.target !== "nav-el") {
+            nav.classList.add('disactive');
+            line1.classList.remove('hide-line');
+            line2.classList.remove('active2');
+            line3.classList.remove('active3');
+        }
+    })
 })
+
+
+
+
